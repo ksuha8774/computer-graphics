6d6005e8f67ecdcb1b9e7a7216281b4af0a0c510
Харчук Ксения <145744526+ksuha8774@users.noreply.github.com>
2025-05-21 17:15:03 +0500

    Update fragment_shader.glsl

--- laba 1/Projectte/fragment_shader.glsl
+++ laba 1/Projectte/fragment_shader.glsl
@@ -1,8 +1,46 @@
 #version 460 core
+in vec3 Normal;
+in vec3 FragPos;
 out vec4 FragColor;
 
-uniform vec4 ourColor;
+// Свойства материала
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
 
-void main() {
-    FragColor = ourColor;
+// Свойства источника света
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+uniform vec3 viewPos;
+
+void main()
+{
+    // Окружающее освещение
+    vec3 ambient = light.ambient * material.ambient;
+    
+    // Диффузное освещение
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    
+    // Зеркальное освещение
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);
+    
+    // Итоговый цвет
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
 }
